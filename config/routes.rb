495792123e0b5114b33e9752277a1d1343594bee
asahisa <98754141+asahisa@users.noreply.github.com>
2022-02-26 06:03:31 +0000

    [Finish] 3-11

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,21 @@ Rails.application.routes.draw do
   #トップページ
   get "/top" => "homes#top"
   #ルーティングの自動生成
-  resources :lists
+  # resources :lists
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # 投稿フォームの新規作成
-  # get "/new" => "lists#new"
-  # # コメント投稿
-  # post "lists" => "lists#create"
-  # #コメント一覧の取得
-  # get "lists" => "lists#index"
-  # #コメントIDの取得,  'lists#show' アクションを "list" としても扱う
-  # get "lists/:id" => "lists#show", as: "list"
-  # #コメントの編集
-  # get "lists/:id/edit" => "lists#edit", as: "edit_list"
-  # #編集したコメントの更新
+  get "/new" => "lists#new"
+  # コメント投稿
+  post "lists" => "lists#create"
+  #コメント一覧の取得
+  get "lists" => "lists#index"
+  #コメントIDの取得,  'lists#show' アクションを "list" としても扱う
+  get "lists/:id" => "lists#show", as: "list"
+  #コメントの編集
+  get "lists/:id/edit" => "lists#edit", as: "edit_list"
+  #編集したコメントの更新
   patch "lists/:id" => "lists#update", as: "update_list"
   # #投稿の削除
   delete "lists/:id" => "lists#destroy", as: "destroy_list"
